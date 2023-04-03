881ee5835e615ae8dc23c12fd48ae9680968448b
Kalash <baisoyakalash@gmail.com>
2023-04-03 16:33:21 +0530

    initial commit

---
+++ src/Post.js
@@ -0,0 +1,22 @@
+export default function Post() {
+    return(
+        <div className="post">
+        <div className="image">
+          <img src="https://t3.ftcdn.net/jpg/05/65/38/58/360_F_565385878_qG5UJYw3XViCwVXZtFnw1u4ywDWAtUYN.jpg" alt="" />
+        </div>
+        <div className="texts">
+          <h2>Full-HD Wallpaper</h2>
+          <p className="info">
+            <a className="author">Kalash Kumar</a>
+            <Date>2023-04-10</Date>
+            <time>17:04</time>
+          </p>
+          <p className="summary">Those videos are likely using emmet. VS Code includes built-in support for emmet completions in html files. For example, typing .classname in an html file will trigger an emmet suggestion that expands to when you accept it
+
+            If you do not see this working:
+
+          </p>
+        </div>
+      </div>
+    );
+}
---
+++ src/pages/LoginPage.js
@@ -0,0 +1,11 @@
+export default function LoginPage(){
+    return(
+        <form className="login">
+            <h1>Login</h1>
+            <input type="text" placeholder="username" />
+            <input type="text" placeholder="username" />
+            <button>Login</button>
+
+        </form>
+    );
+}
